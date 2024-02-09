58382e1456fee3232b26bfd39bdb749271a3d0ea
Olakunle Raheem <olaks.raheem@gmail.com>
2024-02-09 14:36:05 +0100

    Feat: Added selection sort

--- arraysandlists/Program.cs
+++ arraysandlists/Program.cs
@@ -1,43 +1,10 @@
 ﻿
 
 
-Random random = new();
-
-int transportTypesCount = Enum.GetNames(typeof(Transport)).Length;
-
-Transport[][] transport = new Transport[12][];
 
 
-for (int month = 0; month < transport.Length; month++)
-{
-
-    int daysCount = DateTime.DaysInMonth(DateTime.Now.Year, month + 1);
-    transport[month] = new Transport[daysCount];
-    for (int i = 0; i < daysCount; i++)
-    {
-
-        int randomType = random.Next(transportTypesCount);
-        transport[month][i] = (Transport)randomType;
-    }
-}
-
-
-string[] monthNames = Month.GetMonthNames();
-int monthNamesPart = monthNames.Max(n => n.Length) + 2;
-for (int month = 1; month <= transport.Length; month++)
-{
-    Console.Write(
-    $"{monthNames[month - 1]}:".PadRight(monthNamesPart));
-    for (int day = 1; day <= transport[month - 1].Length; day++)
-    {
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.BackgroundColor =
-        transport[month - 1][day - 1].GetColor();
-        Console.Write(transport[month - 1][day - 1].GetChar());
-        Console.BackgroundColor = ConsoleColor.Black;
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.Write(" ");
-    }
-    Console.WriteLine();
-}
+int[] numbers = [10, 11, 90, 4, 32, -10, -56, 76, -90];
+string[] names = ["Anna", "Reinhart", "Paul", "Richard", "Israel"];
+SelectionSort.Sort(names);
+Console.WriteLine(string.Join(",", names));
 
