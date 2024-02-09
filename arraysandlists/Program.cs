7aa574c067e4c9dc5113445434ea1c7b621a541f
Olakunle Raheem <olaks.raheem@gmail.com>
2024-02-09 16:51:21 +0100

    Feat: Understood insertion sort

--- arraysandlists/Program.cs
+++ arraysandlists/Program.cs
@@ -5,6 +5,6 @@
 
 int[] numbers = [10, 11, 90, 4, 32, -10, -56, 76, -90];
 string[] names = ["Anna", "Reinhart", "Paul", "Richard", "Israel"];
-InsertionSort.Sort(names);
-Console.WriteLine(string.Join(",", names));
+InsertionSort.Sort(numbers);
+Console.WriteLine(string.Join(",", numbers));
 
--- arraysandlists/algorithms/InsertionSort.cs
+++ arraysandlists/algorithms/InsertionSort.cs
@@ -13,16 +13,22 @@ public static class InsertionSort
             int j = i;
             while (j > 0 && array[j].CompareTo(array[j - 1]) < 0)
             {
+
                 Swap(array, j, j - 1);
                 j--;
-            }
 
+            }
         }
 
+
     }
+
 
     public static void Swap<T>(T[] array, int first, int second)
     {
-        (array[second], array[first]) = (array[first], array[second]);
+
+        (array[first], array[second]) = (array[second], array[first]);
+
     }
+
 }
